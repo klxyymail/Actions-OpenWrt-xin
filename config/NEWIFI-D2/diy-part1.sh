a9cec8c48b57341b31589b20fea055ae955c50cf
klxyymail <80946422+klxyymail@users.noreply.github.com>
2023-09-04 15:01:46 +0800

    Update diy-part1.sh

--- config/NEWIFI-D2/diy-part1.sh
+++ config/NEWIFI-D2/diy-part1.sh
@@ -14,5 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
