74f09623212cc89e665f98aa39340aa53c441b3e
klxyymail <80946422+klxyymail@users.noreply.github.com>
2023-09-04 14:35:22 +0800

    Create diy-part2.sh

---
+++ config/NEWIFI-D2/diy-part2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP 网关/登录地址
+sed -i 's/192.168.100.1/192.168.100.100/g' package/base-files/files/bin/config_generate
+
+# TTYD 自动登录
+# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+# 设置密码为空（安装固件时无需密码登陆）--设置后没有登录窗口
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-setting
